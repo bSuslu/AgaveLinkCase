abce0cd19a641fc3310978dd496b07f18db72ad3
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-05-09 00:18:36 +0300

    added grid input system

---
+++ Assets/Scripts/GridSystem/GridInputSystem.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+namespace AgaveLinkCase.GridSystem
+{
+    // TODO: new Input system
+    public class GridInputSystem : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        
+        public event Action<Cell> OnNewCellTouched;
+        public event Action OnRelease;
+        
+        private Cell _activeCell;
+        private Grid2D _grid2D;
+        
+        
+        public void Initialize(Grid2D grid2D)
+        {
+            _grid2D = grid2D;
+        }
+        
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                CheckIfActiveCellChanged();
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _activeCell = null;
+                OnRelease?.Invoke();
+            }
+        }
+        
+        private void CheckIfActiveCellChanged()
+        {
+            var mousePosition = Input.mousePosition;
+            var worldPosition = _camera.ScreenToWorldPoint(mousePosition);
+
+            if (_grid2D.TryGetCell(worldPosition, out Cell cell))
+            {
+                if (cell != _activeCell)
+                {
+                    _activeCell = cell;
+                    OnNewCellTouched?.Invoke(cell);
+                }
+            }
+        }
+    }
+}
