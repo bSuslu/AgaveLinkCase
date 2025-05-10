5d92661117c4dd421cf511d5e09e029f387e7104
bSuslu <51022680+bSuslu@users.noreply.github.com>
2025-05-10 20:45:40 +0300

    changed grid settings access modifiers

--- Assets/Scripts/GridSystem/GridSettings.cs
+++ Assets/Scripts/GridSystem/GridSettings.cs
@@ -5,8 +5,9 @@ namespace AgaveLinkCase.GridSystem
     [CreateAssetMenu(fileName = "GridSettings", menuName = "Settings/Grid")]
     public class GridSettings : ScriptableObject
     {
-        [field: SerializeField] public float CellSize;
-        [field: SerializeField] public Vector3 OriginPosition;
-        [field: SerializeField] public bool Debug = true;
+        // TODO: Currently dependent to chip and cell bg size, do add to other calculations to make it accessible
+        public float CellSize = 2.56f;
+        [field: SerializeField] public Vector3 OriginPosition { get; private set; }
+        [field: SerializeField] public bool Debug { get; private set; } = true;
     }
 }
